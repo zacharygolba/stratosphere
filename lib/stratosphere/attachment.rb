67d6aad50a671e8ff4d6e6ca22ad490384838be8
Zachary Golba <zak@zacharygolba.com>
2015-09-23 14:38:59 -0400

    feat: allow for per model config

--- lib/stratosphere/attachment.rb
+++ lib/stratosphere/attachment.rb
@@ -2,22 +2,34 @@ module Stratosphere
   class Attachment
     attr_accessor :base_path, :config, :file_name, :name, :size, :mime_type, :type, :owner, :file_store
 
+    def self.create_config(options={})
+      config = Stratosphere.config.to_hash
+
+      unless options.nil?
+        unless options[:aws].nil?
+          options[:aws] = config[:aws].merge(options[:aws])
+        end
+        config.merge!(options)
+      end
+      Stratosphere::Config.new(config)
+    end
+
     def initialize(owner, name, options={})
-      @config      = Stratosphere.config
-      @name        = name
-      @owner       = owner
-      @file_name   = @owner["#{@name}_file"]
-      @file_size   = @owner["#{@name}_content_length"]
-      @mime_type   = @owner["#{@name}_content_type"]
-      @type        = :attachment
-      @file_store  = Stratosphere::AWS::S3.new
+      @config     = self.class.create_config(options[:config])
+      @name       = name
+      @owner      = owner
+      @file_name  = @owner["#{@name}_file"]
+      @file_size  = @owner["#{@name}_content_length"]
+      @mime_type  = @owner["#{@name}_content_type"]
+      @type       = :attachment
+      @file_store = Stratosphere::AWS::S3.new(@config)
       set_base_path
     end
-    
+
     def set_base_path
       plural_attr  = name.to_s.pluralize
       plural_model = owner.class.to_s.downcase.pluralize
-      
+
       if config.dir_prefix
         prefix     = config.dir_prefix[-1, 1] == '/' ? config.dir_prefix.slice(0, -1) : config.dir_prefix
         @base_path = "#{prefix}/#{plural_model}/#{plural_attr}/#{owner.id}"
@@ -25,7 +37,7 @@ module Stratosphere
         @base_path = "#{plural_model}/#{plural_attr}/#{owner.id}"
       end
     end
-    
+
     def exists?
       !file_name.nil?
     end
